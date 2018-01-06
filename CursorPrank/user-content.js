f60042767466a5e89cb6eafaf1fa68e8dd1aee40
unknown <arikolsh@gmail.com>
2018-01-06 18:39:47 +0200

    "first"

---
+++ CursorPrank/user-content.js
@@ -0,0 +1,11 @@
+function changeCursorStyle(tag,cursorStyle) {
+    cursorStyle = createImageUrl(cursorStyle);
+    $(tag).css('cursor', cursorStyle);
+}
+
+function createImageUrl(cursorStyle) {
+    var imgPath = chrome.extension.getURL(cursorStyle);
+    return 'url(' + imgPath + '), auto';
+}
+
+changeCursorStyle("div","images/balls.png");
